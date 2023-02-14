ef25b15868d4c0432f3cfc9a52b9e477842036ea
mrhappyasthma <mrhappyasthma@gmail.com>
2023-02-13 22:28:45 -0800

    Update zoom addon to support Ashita 4.0
    - Also improved some of the print behavior

--- addons/zoom/zoom.lua
+++ addons/zoom/zoom.lua
@@ -23,9 +23,9 @@
 * No warranties are given.
 ]]--
 
-_addon.author   = 'atom0s';
-_addon.name     = 'zoom';
-_addon.version  = '1.0.0';
+addon.author   = 'atom0s';
+addon.name     = 'zoom';
+addon.version  = '1.0.1';
 
 require 'common'
 
@@ -37,6 +37,13 @@ zoom.pointer1 = 0;
 zoom.pointer2 = 0;
 zoom.backup1 = 0;
 zoom.backup2 = 0;
+zoom.current_zoomout = 0;
+zoom.current_zoomin = 0;
+
+local ZoomType = {
+  IN = 1,
+  OUT = 2
+};
 
 ----------------------------------------------------------------------------------------------------
 -- func: print_help
@@ -44,26 +51,47 @@ zoom.backup2 = 0;
 ----------------------------------------------------------------------------------------------------
 local function print_help(cmd, help)
     -- Print the invalid format header..
-    print('\31\200[\31\05' .. _addon.name .. '\31\200]\30\01 ' .. '\30\68Invalid format for command:\30\02 ' .. cmd .. '\30\01'); 
+    print('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\68Invalid format for command:\30\02 ' .. cmd .. '\30\01'); 
 
     -- Loop and print the help commands..
     for k, v in pairs(help) do
-        print('\31\200[\31\05' .. _addon.name .. '\31\200]\30\01 ' .. '\30\68Syntax:\30\02 ' .. v[1] .. '\30\71 ' .. v[2]);
+        print('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\68Syntax:\30\02 ' .. v[1] .. '\30\71 ' .. v[2]);
     end
+end
+
+----------------------------------------------------------------------------------------------------
+-- func: print_current_zoom_value
+-- desc: Prints the current zoom level.
+----------------------------------------------------------------------------------------------------
+local function print_current_zoom_value(zoom_type)
+  if (zoom_type == ZoomType.IN) then
+    print(string.format('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\07Zoom in:\30\02 %.2f \30\71(Default: 900)\30\01', zoom.current_zoomin)); 
+  elseif (zoom_type == ZoomType.OUT) then
+    print(string.format('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\07Zoom out:\30\02 %.2f \30\71(Default: 242)\30\01', zoom.current_zoomout)); 
+  end
+end
+
+----------------------------------------------------------------------------------------------------
+-- func: print_current_zoom
+-- desc: Prints the current zoom level.
+----------------------------------------------------------------------------------------------------
+local function print_current_zoom()
+  print_current_zoom_value(ZoomType.IN);  
+  print_current_zoom_value(ZoomType.OUT);
 end
 
 ----------------------------------------------------------------------------------------------------
 -- func: load
 -- desc: Event called when the addon is being loaded.
 ----------------------------------------------------------------------------------------------------
-ashita.register_event('load', function()
+ashita.events.register('load', 'load_cb', function()
     -- Locate the needed patterns..
-    local pointer1 = ashita.memory.findpattern('FFXiMain.dll', 0, 'D81D????????DFE0F6C4057A??C7442410????????E8????????8B4C', 0, 0);
-    local pointer2 = ashita.memory.findpattern('FFXiMain.dll', 0, '25????????75??C7442410????????E8????????8B', 0, 0);
+    local pointer1 = ashita.memory.find('FFXiMain.dll', 0, 'D81D????????DFE0F6C4057A??C7442410????????E8????????8B4C', 0, 0);
+    local pointer2 = ashita.memory.find('FFXiMain.dll', 0, '25????????75??C7442410????????E8????????8B', 0, 0);
 
     -- Ensure the pointers are valid..
     if (pointer1 == 0 or pointer2 == 0) then
-        print('\31\200[\31\05' .. _addon.name .. '\31\200]\30\01 ' .. '\30\68Failed to find the required pointers.\30\01');
+        print('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\68Failed to find the required pointers.\30\01');
     end
     
     -- Store the pointers..
@@ -72,18 +100,20 @@ ashita.register_event('load', function()
     
     -- Read the original values to restore later..
     zoom.backup1 = ashita.memory.read_float(zoom.pointer1 + 0x11);
+    zoom.current_zoomout = zoom.backup1;
     zoom.backup2 = ashita.memory.read_float(zoom.pointer2 + 0x0B);
+    zoom.current_zoomin = zoom.backup2;
     
     -- Display the default values..
-    print(string.format('\31\200[\31\05' .. _addon.name .. '\31\200]\30\01 ' .. '\30\07Default zoom out max:\30\02 %f\30\01', zoom.backup1));
-    print(string.format('\31\200[\31\05' .. _addon.name .. '\31\200]\30\01 ' .. '\30\07Default zoom in max:\30\02 %f\30\01', zoom.backup2));
+    print(string.format('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\07Default zoom out max:\30\02 %.2f\30\01', zoom.backup1));
+    print(string.format('\31\200[\31\05' .. addon.name .. '\31\200]\30\01 ' .. '\30\07Default zoom in max:\30\02 %.2f\30\01', zoom.backup2));
 end);
 
 ----------------------------------------------------------------------------------------------------
 -- func: unload
 -- desc: Event called when the addon is being unload.
 ----------------------------------------------------------------------------------------------------
-ashita.register_event('unload', function()
+ashita.events.register('unload', 'unload_cb', function()
     -- Restore the default values..
     if (zoom.pointer1 ~= 0) then
         ashita.memory.write_float(zoom.pointer1 + 0x11, zoom.backup1);
@@ -97,24 +127,33 @@ end);
 -- func: command
 -- desc: Event called when a command was entered.
 ----------------------------------------------------------------------------------------------------
-ashita.register_event('command', function(command, ntype)
+ashita.events.register('command', 'command_cb', function(event)
     -- Get the arguments of the command..
-    local args = command:args();
+    local args = event.command:args();
     if (args[1] ~= '/zoom') then
         return false;
     end
     
+    if (#args == 1) then
+      print_current_zoom();
+      return true;
+    end
+    
     -- Set the zoom out distance..
     if (#args >= 3 and args[2] == 'setout') then
+        zoom.current_zoomout = tonumber(args[3])
         ashita.memory.unprotect(zoom.pointer1 + 0x11, 4);
         ashita.memory.write_float(zoom.pointer1 + 0x11, tonumber(args[3]));
+        print_current_zoom_value(ZoomType.OUT);
         return true;
     end
     
     -- Set the zoom in distance..
     if (#args >= 3 and args[2] == 'setin') then
+        zoom.current_zoomin = tonumber(args[3])
         ashita.memory.unprotect(zoom.pointer2 + 0x0B, 4);
         ashita.memory.write_float(zoom.pointer2 + 0x0B, tonumber(args[3]));
+        print_current_zoom_value(ZoomType.IN);
         return true;
     end
     
@@ -123,5 +162,5 @@ ashita.register_event('command', function(command, ntype)
         { '/zoom setout [num]', '- Sets the distance the camera can zoom out to (using the mouse wheel). (Default: 242)' },
         { '/zoom setin [num]',  '- Sets the distance the camera can zoom into (using the mouse wheel). (Default: 900)' }
     });
-    return true;
+    return false;
 end);
