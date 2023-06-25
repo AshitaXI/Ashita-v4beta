c95403882ad797829ec04556eb154085aea3f723
Sascha Tausend <sascha.tausend@gmail.com>
2023-06-25 10:18:55 +0200

    fixed occasional crashes & added player info

--- addons/petinfo/petinfo.lua
+++ addons/petinfo/petinfo.lua
@@ -20,8 +20,8 @@
 --]]
 
 addon.name      = 'petinfo';
-addon.author    = 'atom0s & Tornac';
-addon.version   = '1.2';
+addon.author    = 'atom0s & Tornac & Skyrant';
+addon.version   = '1.3';
 addon.desc      = 'Displays information about the players pet.';
 addon.link      = 'https://ashitaxi.com/';
 
@@ -112,21 +112,92 @@ ashita.events.register('d3d_present', 'present_cb', function ()
         return;
     end
 
-    -- Obtain the player pet entity..
-    local pet = GetEntity(player.PetTargetIndex);
-    if (pet == nil or pet.Name == nil) then
-        petinfo.target = nil;
-        return;
-    end
-
     imgui.SetNextWindowBgAlpha(0.8);
     imgui.SetNextWindowSize({ 250, -1, }, ImGuiCond_Always);
     if (imgui.Begin('PetInfo', petinfo.is_open, bit.bor(ImGuiWindowFlags_NoDecoration, ImGuiWindowFlags_AlwaysAutoResize, ImGuiWindowFlags_NoSavedSettings, ImGuiWindowFlags_NoFocusOnAppearing, ImGuiWindowFlags_NoNav))) then
+        -- Obtain and prepare player information..
+        local x, _  = imgui.CalcTextSize("99/99");
+        local playerName = GetPlayerEntity().Name;
+        
+        local mainJob = AshitaCore:GetResourceManager():GetString("jobs.names_abbr", AshitaCore:GetMemoryManager():GetPlayer():GetMainJob());
+        local jobLevel = AshitaCore:GetMemoryManager():GetPlayer():GetMainJobLevel();
+        local subJob = AshitaCore:GetResourceManager():GetString("jobs.names_abbr", AshitaCore:GetMemoryManager():GetPlayer():GetSubJob());
+        local subJobLevel = AshitaCore:GetMemoryManager():GetPlayer():GetSubJobLevel();
+        
+        local SelfHP = AshitaCore:GetMemoryManager():GetParty():GetMemberHP(0);
+        local SelfHPMax = AshitaCore:GetMemoryManager():GetPlayer():GetHPMax();
+        local selfHPPercent = SelfHP / SelfHPMax;
+
+        local SelfMP = AshitaCore:GetMemoryManager():GetParty():GetMemberMP(0);
+        local SelfMPMax = AshitaCore:GetMemoryManager():GetPlayer():GetMPMax();
+        local selfMPPercent = SelfMP / SelfMPMax;
+
+        local SelfTP = AshitaCore:GetMemoryManager():GetParty():GetMemberTP(0);
+        local SelfTPPercent = SelfTP / 3000;
+
+        local currentExp = AshitaCore:GetMemoryManager():GetPlayer():GetExpCurrent();
+        local totalExp = AshitaCore:GetMemoryManager():GetPlayer():GetExpNeeded();
+        local expPercent = currentExp / totalExp;
+
+        local inventory = AshitaCore:GetMemoryManager():GetInventory();
+        if (inventory == nil) then
+            UpdateTextVisibility(false);
+            return;
+        end
+
+        local usedBagSlots = inventory:GetContainerCount(0);
+        local maxBagSlots = inventory:GetContainerCountMax(0);
+        local bagSlotPercent = usedBagSlots / maxBagSlots;
+
+        if ( bagSlotPercent > 0.5 ) then
+            bagSlotColor = {0.33,0.66,0.33,1.0};
+        elseif ( bagSlotPercent > 0.75 ) then
+            bagSlotColor = {0.66,0.66,0.33,1.0};
+        else
+            bagSlotColor = {0.33,0.66,0.33,1.0};
+        end
+
+        -- Display the player information..
+        imgui.Text(playerName .. ' ' .. mainJob .. '(' .. jobLevel .. ')' .. '/' .. subJob .. '(' .. subJobLevel .. ')' );
+        imgui.SameLine();
+        imgui.SetCursorPosX(imgui.GetCursorPosX() + imgui.GetColumnWidth() - x - imgui.GetStyle().FramePadding.x);
+        imgui.Text(usedBagSlots .. '/' .. maxBagSlots);
+
+        imgui.Separator();
+        imgui.Text('XP:');
+        imgui.SameLine();
+        imgui.PushStyleColor(ImGuiCol_PlotHistogram, {1.0,0.88,0.33,1.0});
+        imgui.ProgressBar(expPercent, { -1, 16 });
+        imgui.PopStyleColor(1);
+
+        imgui.Separator();
+        imgui.Text('HP:');
+        imgui.SameLine();
+        imgui.ProgressBar(selfHPPercent, { -1, 16 });
+        
+        imgui.Text('MP:');
+        imgui.SameLine();
+        imgui.PushStyleColor(ImGuiCol_PlotHistogram, { 0.0, 0.49, 1.0, 1.0 });
+        imgui.ProgressBar(selfMPPercent, { -1, 16 });
+        imgui.PopStyleColor(1);
+
+        imgui.Text('TP:');
+        imgui.SameLine();
+        imgui.PushStyleColor(ImGuiCol_PlotHistogram, { 0.0, 0.6, 0.14, 1.0 });
+        imgui.ProgressBar(SelfTPPercent, { -1, 16 }, tostring(SelfTP));
+        imgui.PopStyleColor(1);
+        imgui.Separator();
+        
+        -- Obtain the player pet entity..
+        local pet = GetEntity(player.PetTargetIndex);
+        if (pet == nil or pet.Name == nil) then
+            petinfo.target = nil;
+            return;
+        end
         -- Obtain and prepare pet information..
         local petmp = AshitaCore:GetMemoryManager():GetPlayer():GetPetMPPercent();
         local pettp = AshitaCore:GetMemoryManager():GetPlayer():GetPetTP();
         local dist  = ('%.1f'):fmt(math.sqrt(pet.Distance));
-        local x, _  = imgui.CalcTextSize(dist);
 
         -- Display the pets information..
         imgui.Text(pet.Name);
@@ -153,24 +224,25 @@ ashita.events.register('d3d_present', 'present_cb', function ()
             local target = GetEntityByServerId(petinfo.target);
             if (target == nil or target.ActorPointer == 0 or target.HPPercent == 0) then
                 petinfo.target = nil;
+                return;
             else
                 dist = ('%.1f'):fmt(math.sqrt(target.Distance));
                 x, _ = imgui.CalcTextSize(dist);
-
-                local tname = target.Name;
-                if (tname == nil) then
-                    tname = '';
+                
+                if (target.Name ~= nil) then
+                    imgui.Separator();
+                    imgui.Text(target.Name);
+                    imgui.SameLine();
+                    imgui.SetCursorPosX(imgui.GetCursorPosX() + imgui.GetColumnWidth() - x - imgui.GetStyle().FramePadding.x);
+                    imgui.Text(dist);
+                    imgui.Separator();
+                    imgui.Text('HP:');
+                    imgui.SameLine();
+                    imgui.ProgressBar(target.HPPercent / 100, { -1, 16 });
+                else
+                    return;
+                    --imgui.Text('no target');
                 end
-
-                imgui.Separator();
-                imgui.Text(tname);
-                imgui.SameLine();
-                imgui.SetCursorPosX(imgui.GetCursorPosX() + imgui.GetColumnWidth() - x - imgui.GetStyle().FramePadding.x);
-                imgui.Text(dist);
-                imgui.Separator();
-                imgui.Text('HP:');
-                imgui.SameLine();
-                imgui.ProgressBar(target.HPPercent / 100, { -1, 16 });
             end
         end
     end
