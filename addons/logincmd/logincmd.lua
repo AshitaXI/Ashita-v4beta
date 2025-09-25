e06f9e575b7fd4cc2014e3e3404d824992f1c69b
atom0s <atom0s@live.com>
2025-09-25 03:50:03 -0700

    addon: (logincmd) Update copyright. addon: (logincmd) Normalize requires.

--- addons/logincmd/logincmd.lua
+++ addons/logincmd/logincmd.lua
@@ -1,5 +1,5 @@
 --[[
-* Addons - Copyright (c) 2021 Ashita Development Team
+* Addons - Copyright (c) 2025 Ashita Development Team
 * Contact: https://www.ashitaxi.com/
 * Contact: https://discord.gg/Ashita
 *
@@ -21,12 +21,13 @@
 
 addon.name      = 'logincmd';
 addon.author    = 'atom0s';
-addon.version   = '1.0';
+addon.version   = '1.1';
 addon.desc      = 'Executes a per-character script when logging in, or switching characters.';
 addon.link      = 'https://ashitaxi.com/';
 
-require('common');
-local chat = require('chat');
+require 'common';
+
+local chat = require 'chat';
 
 -- LoginCmd Variables
 local logincmd = T{
