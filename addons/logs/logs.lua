2ecc47020f45022978c14118b48d69b761fe19cb
atom0s <atom0s@live.com>
2025-09-25 03:50:27 -0700

    addon: (logs) Update copyright. addon: (logs) Normalize requires.

--- addons/logs/logs.lua
+++ addons/logs/logs.lua
@@ -1,5 +1,5 @@
 --[[
-* Addons - Copyright (c) 2021 Ashita Development Team
+* Addons - Copyright (c) 2025 Ashita Development Team
 * Contact: https://www.ashitaxi.com/
 * Contact: https://discord.gg/Ashita
 *
@@ -21,12 +21,13 @@
 
 addon.name      = 'logs';
 addon.author    = 'atom0s';
-addon.version   = '1.0';
+addon.version   = '1.1';
 addon.desc      = 'Logs all text that goes through the chat log to a file.';
 addon.link      = 'https://ashitaxi.com/';
 
-require('common');
-local chat = require('chat');
+require 'common';
+
+local chat = require 'chat';
 
 -- Logs Variables
 local logs = T{
