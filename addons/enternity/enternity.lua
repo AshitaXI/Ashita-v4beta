d1c8f659b3accfbca8c6bdc928b4abc9deb7a3b7
atom0s <atom0s@live.com>
2025-09-25 03:06:17 -0700

    addon: (enternity) Update copyright. addon: (enternity) Normalize requires.

--- addons/enternity/enternity.lua
+++ addons/enternity/enternity.lua
@@ -1,5 +1,5 @@
 --[[
-* Addons - Copyright (c) 2021 Ashita Development Team
+* Addons - Copyright (c) 2025 Ashita Development Team
 * Contact: https://www.ashitaxi.com/
 * Contact: https://discord.gg/Ashita
 *
@@ -21,12 +21,13 @@
 
 addon.name      = 'enternity';
 addon.author    = 'Hypnotoad & atom0s';
-addon.version   = '1.1';
+addon.version   = '1.2';
 addon.desc      = 'Removes the need to press enter through npc dialog and cutscenes.';
 addon.link      = 'https://ashitaxi.com/';
 
-require('common');
-local chat = require('chat');
+require 'common';
+
+local chat = require 'chat';
 
 -- Enternity Variables
 local enternity = T{
