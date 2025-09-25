5dc71890781723e7b10d5e2d174d50b90c4e0c38
atom0s <atom0s@live.com>
2025-09-25 04:15:04 -0700

    addon: (skeletonkey) Update copyright. addon: (skeletonkey) Normalize requires.

--- addons/skeletonkey/skeletonkey.lua
+++ addons/skeletonkey/skeletonkey.lua
@@ -1,5 +1,5 @@
 --[[
-* Addons - Copyright (c) 2021 Ashita Development Team
+* Addons - Copyright (c) 2025 Ashita Development Team
 * Contact: https://www.ashitaxi.com/
 * Contact: https://discord.gg/Ashita
 *
@@ -21,12 +21,13 @@
 
 addon.name      = 'skeletonkey';
 addon.author    = 'atom0s';
-addon.version   = '1.0';
+addon.version   = '1.1';
 addon.desc      = 'Enables the ability to force closed doors open.';
 addon.link      = 'https://ashitaxi.com/';
 
-require('common');
-local chat = require('chat');
+require 'common';
+
+local chat = require 'chat';
 
 -- Skeletonkey Variables
 local skeletonkey = {
