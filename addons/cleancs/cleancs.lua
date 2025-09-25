b8576186f3ff61870500c28c6a12c9f1336f19a8
atom0s <atom0s@live.com>
2025-09-25 02:55:58 -0700

    addon: (cleancs) Update copyright. addon: (cleancs) Normalize requires.

--- addons/cleancs/cleancs.lua
+++ addons/cleancs/cleancs.lua
@@ -1,5 +1,5 @@
 --[[
-* Addons - Copyright (c) 2021 Ashita Development Team
+* Addons - Copyright (c) 2025 Ashita Development Team
 * Contact: https://www.ashitaxi.com/
 * Contact: https://discord.gg/Ashita
 *
@@ -21,11 +21,11 @@
 
 addon.name      = 'cleancs';
 addon.author    = 'atom0s';
-addon.version   = '1.0';
+addon.version   = '1.1';
 addon.desc      = 'Hides Ashita rendered elements while in a cutscene.';
 addon.link      = 'https://ashitaxi.com/';
 
-require('common');
+require 'common';
 
 -- CleanCS Variables
 local cleancs = T{
